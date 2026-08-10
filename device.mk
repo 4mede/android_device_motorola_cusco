d10eef23a0340069693fd303a34e535521ea65a8
4mede <4mede@tutamail.com>
2026-08-10 13:06:27 -0400

    cusco: Build sm7435-common fingerprint service

--- device.mk
+++ device.mk
@@ -41,6 +41,10 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
         
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/init.recovery.touch.rc:recovery/root/init.recovery.touch.rc
+    
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.moto_sm7435_fod
 
 # LiveDisplay
 $(call soong_config_set_bool,livedisplay_sysfs,enable_af,true)
